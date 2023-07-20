9bbd03ef64c28b1cfe6ca6b18614e2067ee606c0
kulturman <arnaudbakyono@gmail.com>
2023-07-20 14:38:43 +0000

    Implement home page

--- frontend/src/app/pages/home-page/home-page.component.ts
+++ frontend/src/app/pages/home-page/home-page.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent {
-
+  usefulLinks = [
+    {
+      img: 'assets/images/usefulLinks/gafi.jpeg',
+      link: 'https://www.fatf-gafi.org/',
+      text: 'GAFI'
+    },
+    {
+      img: 'assets/images/usefulLinks/giaba.jpeg',
+      link: 'https://giaba.org',
+      text: 'GIABA'
+    },
+    {
+      img: 'assets/images/usefulLinks/onu.jpeg',
+      link: 'https://www.unodc.org/unodc/fr/',
+      text: 'ONUDC'
+    },
+    {
+      img: 'assets/images/logo.jpeg',
+      link: '',
+      text: 'DECLARATION EN LIGNE'
+    },
+    {
+      img: 'assets/images/logo.jpeg',
+      link: '',
+      text: 'AUTORITE DE SUPERVISION ET DE CONTROLE'
+    },
+    {
+      img: 'assets/images/usefulLinks/onu.jpeg',
+      link: 'https://www.un.org/securitycouncil/fr/content/un-sc-consolidated-list',
+      text: 'LISTES SFC DES NATIONS UNIES'
+    },
+  ]
 }
